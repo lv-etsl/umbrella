7987523482d43cf6f795f219ba4448d78f23703b
lv-etsl <44968083+lv-etsl@users.noreply.github.com>
2019-06-03 16:05:49 +0200

    update

--- inc/widgets.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// the widgets file: on y liste les divers widgets du thèmes
-
-register_sidebar( array(
-    'id'          => 'left-sidebar',
-    'name'        => 'Sidebar Gauche',
-    'description' => 'C est la sidebar qui s affiche à gauche',
-) );
